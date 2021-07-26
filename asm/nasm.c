471b8c9246b7df24e8b08cf1855708297d34a87a
Byoungchan Lee <daniel.l@hpcnt.com>
2021-07-26 20:09:09 +0900

    Emit the LC_BUILD_VERSION load command when creating a mach object.
    LC_BUILD_VERSION contains the min OS version on which this binary
    was built to run for its platform in mach object. It is required for
    targets like iOS Catalyst.
    
    Also, emit the __LLVM segment, __asm section to tell the Apple
    toolchain that this object is from assembler and has no bitcode.
    This trick is used in Kotlin/Native, Rust, Flutter, Golang and yasm.

--- asm/nasm.c
+++ asm/nasm.c
@@ -166,6 +166,10 @@ static char *quote_for_pmake(const char *str);
 static char *quote_for_wmake(const char *str);
 static char *(*quote_for_make)(const char *) = quote_for_pmake;
 
+#if defined(OF_MACHO) || defined(OF_MACHO64)
+extern bool macho_set_min_os(const char *str);
+#endif
+
 /*
  * Execution limits that can be set via a command-line option or %pragma
  */
@@ -947,7 +951,8 @@ enum text_options {
     OPT_KEEP_ALL,
     OPT_NO_LINE,
     OPT_DEBUG,
-    OPT_REPRODUCIBLE
+    OPT_REPRODUCIBLE,
+    OPT_MACHO_MIN_OS
 };
 enum need_arg {
     ARG_NO,
@@ -980,6 +985,7 @@ static const struct textargs textopts[] = {
     {"no-line",  OPT_NO_LINE, ARG_NO, 0},
     {"debug",    OPT_DEBUG, ARG_MAYBE, 0},
     {"reproducible", OPT_REPRODUCIBLE, ARG_NO, 0},
+    {"macho-min-os", OPT_MACHO_MIN_OS, ARG_YES, 0},
     {NULL, OPT_BOGUS, ARG_NO, 0}
 };
 
@@ -1349,6 +1355,23 @@ static bool process_arg(char *p, char *q, int pass)
                     break;
                 case OPT_REPRODUCIBLE:
                     reproducible = true;
+                    break;
+                case OPT_MACHO_MIN_OS:
+                    if (pass == 2) {
+                        if (strstr(ofmt->shortname, "macho") != ofmt->shortname) {
+                            nasm_error(
+                                ERR_WARNING | WARN_OTHER | ERR_USAGE,
+                                "macho-min-os is only valid for macho format, current: %s",
+                                ofmt->shortname);
+                    break;
+                        }
+#if defined(OF_MACHO) || defined(OF_MACHO64)
+                        if (!macho_set_min_os(param)) {
+                            nasm_fatalf(ERR_USAGE, "failed to set minimum os for mach-o '%s'",
+                                        param);
+                        }
+#endif
+                    }
                     break;
                 case OPT_HELP:
                     /* Allow --help topic without *requiring* topic */
@@ -2517,6 +2540,7 @@ static void help(FILE *out, const char *what)
             "    --lprefix str  prepend the given string to local symbols\n"
             "    --lpostfix str append the given string to local symbols\n"
             "    --reproducible attempt to produce run-to-run identical output\n"
+            "    --macho-min-os minos minimum os version for mach-o format(example: macos-11.0)\n"
             , out);
     }
     if (help_optor(with, HW_LIMIT)) {
